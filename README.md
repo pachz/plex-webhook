646b5eb49dc52bdcf5c9a4de529605cec05c6edc
Payam Chamani <pchamani@nizek.com>
2021-01-12 23:16:09 +0330

    yeay

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# plex-webhook
-
-In order to run this app: 
+In order to run this app:
+ 
 - Install [node.js](https://nodejs.org/en/).
 - Install [yarn](https://yarnpkg.com/en/docs/install).
 - Sign up for a free [ipstack API key](https://ipstack.com/signup/free).
@@ -9,7 +8,6 @@ In order to run this app:
 - Make a new app at Heroku, and add the Heroku Redis add-on (free plan) and note the app URL.
 - Add a config var APP_URL (usually {app_name}.herokuapp.com)
 - Make a Slack webhook for a slack-channel and note the URL, add them as config vars named SLACK_URL & SLACK_CHANNEL.
-- Make a Discord bot add the config vars named DISCORD_CHANNEL_ID & DISCORD_TOKEN.
 - Deploy to Heroku.
 - Have anyone who wants to contribute add the webhook on https://app.plex.tv/web/app#!/account/webhooks
 
@@ -18,16 +16,9 @@ Alternatively, deploy straight to Heroku now:
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
 
 You'll be asked to complete these config vars
-```dotenv
-APP_URL                         # URL of this app
-IPSTACK_KEY                     # API key from ipstack.com
-REDIS_URL                       # The URL of redis (will be configured automatically)
-SLACK_URL                       # URL of Slack WebHook
-SLACK_CHANNEL                   # Channel name of Slack [#channel-name]
-DISCORD_CHANNEL_ID              # Channel ID of Discord
-DISCORD_TOKEN                   # Token to access Discord
-POST_TO_DISCORD                 # Defines if the app can post to discord [bool]
-POST_TO_SLACK                   # Defines if the app can post to slack [bool]
-ANONYMIZE_USER_FOR_DISCORD      # Defines if the post should be anonymize the plex user-name [bool]
-ANONYMIZE_USER_FOR_SLACK        # Defines if the post should be anonymize the plex user-name [bool]
+```
+SLACK_URL      # your slack webhook URL
+SLACK_CHANNEL  # the slack #channel-name to post messages
+APP_URL        # the App URL ({app_name}.herokuapp.com)
+IPSTACK_KEY    # your ipstack API key
 ```
--- index.js
+++ index.js
@@ -6,439 +6,200 @@ const Redis = require('ioredis');
 const request = require('request-promise-native');
 const sha1 = require('sha1');
 const Slack = require('slack-node');
-const upload = multer({storage: multer.memoryStorage()});
-const Discord = require('discord.js');
+const upload = multer({ storage: multer.memoryStorage() });
 
 const SEVEN_DAYS = 7 * 24 * 60 * 60; // in seconds
 
-// DKTODO: convert console.logs to slack notifications
-
 //
-// playback events
-
-const MEDIA_PLAYING = 'media.play';
-const MEDIA_PAUSED = 'media.pause';
-const MEDIA_RESUMED = 'media.resume';
-const MEDIA_STOPPED = 'media.stop';
-const MEDIA_VIEWED = 'media.scrobble';
-const MEDIA_RATED = 'media.rate';
-
-//
-// library events
-
-const LIBRARY_MEDIA_ADDED = 'library.new';
-const LIBRARY_MEDIA_ADDED_ON_DECK = 'library.on.deck';
-
-//
-// server owner events
-
-const ADMIN_DATABASE_BACKUP = 'admin.database.backup';
-const ADMIN_DATABASE_CORRUPTED = 'admin.database.corrupted';
-const NEW_DEVICE = 'device.new';
-const PLAYBACK_STARTED = 'playback.started';
-
-//
-// app config
+// setup
 
 const appURL = process.env.APP_URL;
-const port = process.env.PORT || 11000;
-const redisUrl = process.env.REDIS_URL;
+const redis = new Redis(process.env.REDIS_URL);
 
 //
-// slack config
+// slack
 
-const postToSlack = process.env.POST_TO_SLACK || false;
-const anonymizeUserForSlack = process.env.ANONYMIZE_USER_FOR_SLACK || true; // DKTODO: use config
-const slackUrl = process.env.SLACK_URL;
-const slackChannel = process.env.SLACK_CHANNEL;
+//const slack = new Slack();
+//slack.setWebhook(process.env.SLACK_URL);
 
-//
-// discord config
-
-const postToDiscord = process.env.POST_TO_DISCORD || false;
-const anonymizeUserForDiscord = process.env.ANONYMIZE_USER_FOR_DISCORD || true; // DKTODO: use config
-const discordChannel = process.env.DISCORD_CHANNEL_ID;
-const discordToken = process.env.DISCORD_TOKEN;
+const slack = {};
 
 //
-// init slack
-
-const slack = new Slack();
-slack.setWebhook(slackUrl);
-
-//
-// init discord
-
-const discordClient = new Discord.Client();
-discordClient.login(discordToken);
-
-//
-// init
+// express
 
 const app = express();
-const redis = new Redis(redisUrl);
-
-//
-// start app
+const port = process.env.PORT || 11000;
 
 app.use(morgan('dev'));
-app.listen(port, () => console.log(`Express app running at http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`Express app running at http://localhost:${port}`);
+});
 
 //
 // routes
 
-//
-// main route
-
 app.post('/', upload.single('thumb'), async (req, res, next) => {
-    const payload = JSON.parse(req.body.payload);
+  const payload = JSON.parse(req.body.payload);
 
-    const isVideo = isLibrarySectionTypeVideo(payload);
-    const isMusic = isLibrarySectionTypeMusic(payload);
-    const key = generateImageKey(payload);
+  const isVideo = (['movie', 'episode'].includes(payload.Metadata.type));
+  const isAudio = (payload.Metadata.type === 'track');
+  const key = sha1(payload.Server.uuid + payload.Metadata.ratingKey);
 
-    // missing required properties
-    if (!payload.Metadata || !(isMusic || isVideo)) {
-        console.error('[APP]', `Missing required properties`);
+  // missing required properties
+  if (!payload.user || !payload.Metadata || !(isAudio || isVideo)) {
+    return res.sendStatus(400);
+  }
 
-        // DKTODO: temporary logging to slack
-        slack.webhook(
-            {
-                slackChannel,
-                username: 'Plex',
-                icon_emoji: ':plex:',
-                attachments: [{
-                    color: '#a67a2d',
-                    title: 'Debugging',
-                    text: req.body.payload
-                }]
-            },
-            () => null
-        );
+  // retrieve cached image
+  let image = await redis.getBuffer(key);
 
-        next(createErrorMessage(400, 'Bad Request'));
-        return;
-    }
-
-    if (isMediaPause(payload.event) || isMediaStop(payload.event) || isMediaResume(payload.event) || isPlaybackStarted(payload.event)) {
-        console.warn('[APP]', `Event type is: "${payload.event}".  Will be ignored.`);
-
-        return res.json(createMessage(200, 'OK'));
-    }
-
-    if (
-        isNewMediaAdded(payload.event) || isNewMediaAddedOnDeck(payload.event)
-        || isNewDeviceAdded(payload.event)
-        || isDatabaseBackupCompleted(payload.event) || isDatabaseCorrupted(payload.event)
-    ) {
-        console.warn('[APP]', `Event type is: "${payload.event}".  Will be ignored.`);
-
-        return res.json(createMessage(200, 'OK'));
-    }
-
-
-    // retrieve cached image
-    let image = await redis.getBuffer(key);
-
-    // save new image
-    if (!image && req.file && req.file.buffer) {
-        console.log('[REDIS]', `Saving new image ${key}`);
-        image = await sharp(req.file.buffer)
-            .resize(75, 75)
-            .background('white')
-            .embed()
-            .toBuffer();
-
-        redis.set(key, image, 'EX', SEVEN_DAYS);
+  // save new image
+  if (payload.event === 'media.play' || payload.event === 'media.rate') {
+    if (image) {
+      console.log('[REDIS]', `Using cached image ${key}`);
     } else {
-        console.log('[REDIS]', `Using cached image ${key}`);
+      let buffer;
+      if (req.file && req.file.buffer) {
+        buffer = req.file.buffer;
+      } else if (payload.thumb) {
+        console.log('[REDIS]', `Retrieving image from  ${payload.thumb}`);
+        buffer = await request.get({
+          uri: payload.thumb,
+          encoding: null
+        });
+      }
+      if (buffer) {
+        image = await sharp(buffer)
+          .resize({
+            height: 75,
+            width: 75,
+            fit: 'contain',
+            background: 'white'
+          })
+          .toBuffer();
+
+        console.log('[REDIS]', `Saving new image ${key}`);
+        redis.set(key, image, 'EX', SEVEN_DAYS);
+      }
+    }
+  }
+
+  // post to slack
+  if ((payload.event === 'media.scrobble' && isVideo) || payload.event === 'media.rate') {
+    const location = await getLocation(payload.Player.publicAddress);
+
+    let action;
+
+    if (payload.event === 'media.scrobble') {
+      action = 'played';
+    } else if (payload.rating > 0) {
+      action = 'rated ';
+      for (var i = 0; i < payload.rating / 2; i++) {
+        action += ':star:';
+      }
+    } else {
+      action = 'unrated';
     }
 
-    let location = '';
-
-    if (isVideo && payload.Player && payload.Player.publicAddress) {
-        location = await getLocation(payload.Player.publicAddress);
+    if (image) {
+      console.log('[SLACK]', `Sending ${key} with image`);
+      notifySlack(appURL + '/images/' + key, payload, location, action);
+    } else {
+      console.log('[SLACK]', `Sending ${key} without image`);
+      notifySlack(null, payload, location, action);
     }
+  }
 
-    const action = getAction(payload);
+  res.sendStatus(200);
 
-    // post to slack
-    if (postToSlack) {
-        if (image) {
-            console.log('[SLACK]', `Sending ${key} with image`);
-            notifySlack(appURL + '/images/' + key, payload, location, action);
-        } else {
-            console.log('[SLACK]', `Sending ${key} without image`);
-            notifySlack(null, payload, location, action);
-        }
-    }
-
-    // post to discord
-    if (postToDiscord) {
-        if (image) {
-            console.log('[DISCORD]', `Sending ${key} with image`);
-            notifyDiscord(appURL + '/images/' + key, payload, location, action);
-        } else {
-            console.log('[DISCORD]', `Sending ${key} without image`);
-            notifyDiscord(null, payload, location, action);
-        }
-    }
-
-    return res.json(createMessage(200, 'OK'));
 });
-
-//
-// image route
 
 app.get('/images/:key', async (req, res, next) => {
-    const exists = await redis.exists(req.params.key);
+  const exists = await redis.exists(req.params.key);
 
-    if (!exists) {
-        return next();
-    }
+  if (!exists) {
+    return next();
+  }
 
-    const image = await redis.getBuffer(req.params.key);
-    sharp(image).jpeg().pipe(res);
-});
-
-//
-// ping route
-
-app.get('/ping', async (req, res, next) => {
-    return res.json(createMessage(200, 'OK'));
+  const image = await redis.getBuffer(req.params.key);
+  sharp(image).jpeg().pipe(res);
 });
 
 //
 // error handlers
 
 app.use((req, res, next) => {
-    next(createErrorMessage(404, 'Not Found'));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 app.use((err, req, res, next) => {
-    const statusCode = err.status || 500;
-    res.status(statusCode);
-    res.json(generateErrorResponse(statusCode, err.message));
+  res.status(err.status || 500);
+  res.send(err.message);
 });
-
-//
-// do not let app sleep
-
-setInterval(() => request.get(`${appURL}/ping`), 300000);
 
 //
 // helpers
 
 function getLocation(ip) {
-    return request.get(`http://api.ipstack.com/${ip}?access_key=${process.env.IPSTACK_KEY}`, {json: true});
+  return request.get(`http://api.ipstack.com/${ip}?access_key=${process.env.IPSTACK_KEY}`, { json: true });
 }
 
 function formatTitle(metadata) {
-    if (metadata.grandparentTitle) {
-        return metadata.grandparentTitle;
-    }
-
+  if (metadata.grandparentTitle) {
+    return metadata.grandparentTitle;
+  } else {
     let ret = metadata.title;
-
     if (metadata.year) {
-        ret += ` (${metadata.year})`;
+      ret += ` (${metadata.year})`;
     }
-
     return ret;
+  }
 }
 
 function formatSubtitle(metadata) {
-    let ret = '';
+  let ret = '';
 
-    if (metadata.grandparentTitle) {
-        if (metadata.type === 'track') {
-            ret = metadata.parentTitle;
-        } else if (metadata.index && metadata.parentIndex) {
-            ret = `S${metadata.parentIndex} E${metadata.index}`;
-        } else if (metadata.originallyAvailableAt) {
-            ret = metadata.originallyAvailableAt;
-        }
-
-        if (metadata.title) {
-            ret += ' - ' + metadata.title;
-        }
-    } else if (metadata.type === 'movie') {
-        ret = metadata.tagline;
+  if (metadata.grandparentTitle) {
+    if (metadata.type === 'track') {
+      ret = metadata.parentTitle;
+    } else if (metadata.index && metadata.parentIndex) {
+      ret = `S${metadata.parentIndex} E${metadata.index}`;
+    } else if (metadata.originallyAvailableAt) {
+      ret = metadata.originallyAvailableAt;
     }
 
-    return ret;
+    if (metadata.title) {
+      ret += ' - ' + metadata.title;
+    }
+  } else if (metadata.type === 'movie') {
+    ret = metadata.tagline;
+  }
+
+  return ret;
 }
 
 function notifySlack(imageUrl, payload, location, action) {
-    let locationText = '';
+  let locationText = '';
 
-    if (location) {
-        const state = location.country_code === 'US' ? location.region_name : location.country_name;
-        locationText = `near ${location.city}, ${state}`;
-    }
+  if (location) {
+    const state = location.country_code === 'US' ? location.region_name : location.country_name;
+    locationText = `near ${location.city}, ${state}`;
+  }
 
-    // DKTODO: temporary fix
-    const title = formatTitle(payload.Metadata);
-
-    slack.webhook(
-        {
-            slackChannel,
-            username: 'Plex',
-            icon_emoji: ':plex:',
-            attachments: [{
-                fallback: `${title} ${action} by ${payload.Account.title}`,
-                color: '#a67a2d',
-                title: formatTitle(payload.Metadata),
-                text: formatSubtitle(payload.Metadata),
-                thumb_url: imageUrl,
-                footer: `${action} by ${payload.Account.title} on ${payload.Player.title} from ${payload.Server.title} ${locationText}`,
-                footer_icon: payload.Account.thumb
-            }]
-        },
-        () => null
-    );
-}
-
-function notifyDiscord(imageUrl, payload, location, action) {
-    let locationText = '';
-
-    if (location) {
-        const state = location.country_code === 'US' ? location.region_name : location.country_name;
-        locationText = `near ${location.city}, ${state}`;
-    }
-
-    discordClient.channels.get(discordChannel).send({
-        embed: {
-            color: 3447003,
-            title: formatTitle(payload.Metadata),
-            description: formatSubtitle(payload.Metadata),
-            timestamp: new Date(),
-            footer: {
-                icon_url: 'https://dl2.macupdate.com/images/icons256/42311.png?d=1535042731',
-                text: `${action} from ${payload.Server.title} ${locationText}`
-            }
-        }
-    });
-}
-
-function isMediaPlay(mediaEvent) {
-    return mediaEvent === MEDIA_PLAYING;
-}
-
-function isMediaPause(mediaEvent) {
-    return mediaEvent === MEDIA_PAUSED;
-}
-
-function isMediaResume(mediaEvent) {
-    return mediaEvent === MEDIA_RESUMED;
-}
-
-function isMediaStop(mediaEvent) {
-    return mediaEvent === MEDIA_STOPPED;
-}
-
-function isMediaScrobble(mediaEvent) {
-    return mediaEvent === MEDIA_VIEWED;
-}
-
-// DKTODO: only showing "Season Nr."
-function isMediaRate(mediaEvent) {
-    return mediaEvent === MEDIA_RATED;
-}
-
-function isNewMediaAdded(mediaEvent) {
-    return mediaEvent === LIBRARY_MEDIA_ADDED;
-}
-
-function isNewMediaAddedOnDeck(mediaEvent) {
-    return mediaEvent === LIBRARY_MEDIA_ADDED_ON_DECK;
-}
-
-function isDatabaseBackupCompleted(mediaEvent) {
-    return mediaEvent === ADMIN_DATABASE_BACKUP;
-}
-
-function isDatabaseCorrupted(mediaEvent) {
-    return mediaEvent === ADMIN_DATABASE_CORRUPTED;
-}
-
-function isNewDeviceAdded(mediaEvent) {
-    return mediaEvent === NEW_DEVICE;
-}
-
-function isPlaybackStarted(mediaEvent) {
-    return mediaEvent === PLAYBACK_STARTED;
-}
-
-function getAction(payload) {
-    switch (payload.event) {
-        case MEDIA_PLAYING:
-            return 'playing';
-
-        case MEDIA_PAUSED:
-            return 'paused';
-
-        case MEDIA_RESUMED:
-            return 'resumed';
-
-        case MEDIA_STOPPED:
-            return 'stopped';
-
-        case MEDIA_VIEWED:
-            return 'viewed';
-
-        case MEDIA_RATED:
-            return getValueForMediaRated(payload);
-
-        default:
-            console.error('[APP]', `Unknown event: "${payload.event}"`);
-            return 'unknown';
-    }
-}
-
-function getValueForMediaRated(payload) {
-    let ratedAction = 'rated';
-
-    if (payload.rating > 0) {
-        ratedAction += ' ';
-        for (let i = 0; i < payload.rating / 2; i++) {
-            ratedAction += ':star:';
-        }
-    }
-
-    return ratedAction;
-}
-
-function isLibrarySectionTypeVideo(payload) {
-    return payload.Metadata.librarySectionType === 'movie' || payload.Metadata.librarySectionType === 'show';
-}
-
-function isLibrarySectionTypeMusic(payload) {
-    return payload.Metadata.librarySectionType === 'artist';
-}
-
-function generateImageKey(payload) {
-    return sha1(payload.Server.uuid + payload.Metadata.ratingKey);
-}
-
-function generateErrorResponse(statusCode, message) {
-    return {
-        errors: [
-            {
-                status: statusCode,
-                message: message,
-            }
-        ]
-    };
-}
-
-function createErrorMessage(statusCode, message) {
-    const err = new Error(message);
-    err.status = statusCode;
-
-    return err;
-}
-
-function createMessage(statusCode, message) {
-    return {status: statusCode, message: message};
+  slack.webhook({
+    channel,
+    username: 'Plex',
+    icon_emoji: ':plex:',
+    attachments: [{
+      fallback: 'Required plain-text summary of the attachment.',
+      color: '#a67a2d',
+      title: formatTitle(payload.Metadata),
+      text: formatSubtitle(payload.Metadata),
+      thumb_url: imageUrl,
+      footer: `${action} by ${payload.Account.title} on ${payload.Player.title} from ${payload.Server.title} ${locationText}`,
+      footer_icon: payload.Account.thumb
+    }]
+  }, () => {});
 }
