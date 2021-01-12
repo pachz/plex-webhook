50b3408d2b50a845166ab15a20ade6833c988ee9
Administrator <pchamani@nizek.com>
2021-01-13 00:29:50 +0330

    a

--- index.js
+++ index.js
@@ -15,12 +15,34 @@ const SEVEN_DAYS = 7 * 24 * 60 * 60; // in seconds
 
 const appURL = process.env.APP_URL;
 const redis = new Redis(process.env.REDIS_URL);
+const TOKEN = process.env.T_TOKEN || 'XXX';
 
-//
-// slack
+const TelegramBot = require('node-telegram-bot-api');
 
-// const slack = new Slack();
-// slack.setWebhook(process.env.SLACK_URL);
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+  console.log(msg);
+});
 
 const slack = {};
 
